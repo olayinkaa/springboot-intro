b28ee37f577c66831aba4cc049b85ece60a61b5e
Olayinka Ibrahim <olayinka.ibrahim@etranzact.com>
2019-12-03 17:30:23 +0100

    create users

--- src/main/java/com/example/restservices/controllers/UserController.java
+++ src/main/java/com/example/restservices/controllers/UserController.java
@@ -1,9 +1,13 @@
 package com.example.restservices.controllers;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.restservices.entities.User;
@@ -33,8 +37,20 @@ public class UserController {
 	}
 	
 	
+	@PostMapping("/users")
+	public User createUser(@RequestBody User user)
+	{
+		
+		return userService.createUser(user);
+	}
 	
+//getUserById
 	
+	@GetMapping("/users/{id}")
+	public Optional<User> getUserById(@PathVariable("id") Long id)
+	{
+		return userService.getUserById(id);
+	}
 	
 	
 	
